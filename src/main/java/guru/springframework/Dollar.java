679b4170642264235b3cb6520a14b5a78302c208
eduard.streltsov <streltsov.eduard@gmail.com>
2018-11-15 19:03:25 +0200

    S2L19

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,24 @@
+package guru.springframework;
+
+public class Dollar {
+	
+	private int amount;
+	
+	public int getAmount() {
+		return amount;
+	}
+	
+	public Dollar(int amount){
+		this.amount = amount;
+	}
+	
+	public Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Dollar dollar = (Dollar) obj;
+		return this.amount == dollar.amount;
+	}
+}
