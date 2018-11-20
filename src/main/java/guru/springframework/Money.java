35d48b0617cf131dd765709ee267dc411afd213c
Eduard Streltsov <streltsov.eduard@gmail.com>
2018-11-20 21:19:02 +0200

    S3L25

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,6 +1,8 @@
 package guru.springframework;
 
-public abstract class Money {
+import java.util.Objects;
+
+public class Money {
 	
 	protected int amount;
 	protected String currency;
@@ -15,19 +17,36 @@ public abstract class Money {
 	}
 	
 	public static Money dollar(int amount) {
-		return new Dollar(amount, "USD");
+		return new Money(amount, "USD");
 	}
 	
 	public static Money franc(int amount) {
-		return new Franc(amount, "CHF");
+		return new Money(amount, "CHF");
 	}
 	
-	public abstract Money times(int multiplier);
+	public Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
+	}
 	
 	@Override
-	public boolean equals(Object obj) {
-		Money money = (Money) obj;
-		return this.amount == money.amount
-				       && this.getClass().equals(obj.getClass());
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		Money money = (Money) o;
+		return amount == money.amount &&
+				       this.currency.equals(money.currency);
+	}
+	
+	@Override
+	public int hashCode() {
+		
+		return Objects.hash(amount, currency);
+	}
+	
+	@Override
+	public String toString() {
+		return "Money{" +
+				       "amount=" + amount +
+				       ", currency='" + currency + '\'' +
+				       '}';
 	}
 }
